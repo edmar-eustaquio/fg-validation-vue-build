af0a402174d9c2c3e10c6a55269a86676abd549f
Newton <newtonprog3@gmail.com>
2025-10-10 16:06:49 +0800

    10102025 update 2

---
+++ assets/validator-13db6b57.js
@@ -0,0 +1,1 @@
+import{W as i}from"./AuthenticatedLayout-6c76d90f.js";const n=o=>["",...i].includes(o);export{n as i};
--- assets/validator-fd88d94b.js
+++
@@ -1,1 +0,0 @@
-import{W as i}from"./AuthenticatedLayout-50ff9968.js";const n=o=>["",...i].includes(o);export{n as i};
